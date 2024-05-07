8a6703683824073ff00d5633cde8fed6285d6cde
nyleLarsen <nylarseen@gmail.com>
2024-05-07 14:16:06 -0600

    finalProject

--- test/pageobjects/topLinks.js
+++ test/pageobjects/topLinks.js
@@ -16,30 +16,42 @@ class ProofOfConcept extends Url {
     get page404() {
         return $('//*[contains(text(),"404 Not Found")]')
     }
+    get page() {
+        return $('html');
+    }
     async proofRun() {
         await this.openUrl('');
         //console.log(await this.options(1) + "cheese1");
         let dropLinkLength = await this.drop().length;
         //console.log(length + "cheese2");
         //console.log(await this.drop(1) + "cheese3")
-        for(let y = 0;y < dropLinkLength; y++){
+        for(let y = 0;y < 1/*dropLinkLength*/; y++){
             let optGrab = this.options(y+1);
             let insLinkLength = await this.options(y+1).length;
             for(let i = 0;i < insLinkLength; i++){
                 let chgTab = await browser.getWindowHandles();
                 //console.log(chgTab + "cheese5")
                 await this.openUrl('');
+                let origUrl = browser.getUrl();
                 if(chgTab.length > 1){
                     await browser.switchToWindow(chgTab[0]);
                 }
                 //console.log(y + "cheese4");
                 (await this.drop()[y]).click();
                 //await browser.pause(500);
+                await optGrab[i].waitForStable();
                 (await optGrab[i]).click();
-                if((!await this.badPage || !await this.page404) == true){
-                    throw new Error(`bad link detected on dropdown ${y+1}, link ${i+1}.`)
-                }
-                await browser.pause(300);
+                await browser.waitUntil(async () => {
+                    let linkUrl = await browser.getUrl();
+                    return linkUrl !== origUrl;
+                }, {
+                    timeout: 2000,
+                    timeoutMsg: "link doesn't work or the link brings user to same page"
+                });
+                // if((await this.badPage).isExisting() || (await this.page404).isExisting()){
+                //     throw new Error(`bad link detected on dropdown ${y+1}, link ${i+1}.`)
+                // }
+                //await browser.pause(300);
             }
         }
     }
